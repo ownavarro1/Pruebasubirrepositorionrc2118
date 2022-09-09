27ede6173233b7421e720ae0d7e72b08b7f7d7d3
ownavarro <ownavarro1@gmail.com>
2022-09-09 09:35:16 -0500

    PruebaSubirRepositorio

---
+++ GeneradorVertebrados.java
@@ -0,0 +1,10 @@
+public class GeneradorVertebrados {
+    public static Vertebrado[] generarArrayVertebrados(){
+        Vertebrado[] miVertebrados = new Vertebrado[4];
+        miVertebrados[0] = new Vertebrado("Came-llo", "Ma-mifero"); 
+        miVertebrados[1] = new Vertebrado("Vac-a", "Mamifer-o");
+        miVertebrados[2] = new Vertebrado("Tibu-ron", "Pe-z"); 
+        
+        return miVertebrados;
+    }
+}
